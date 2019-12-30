b0468048376dc699b5e3434913c93d41cc214b14
rajan-mishra <56820192+rajan-mishra@users.noreply.github.com>
2019-12-30 18:58:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,36 +12,36 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/login')
+@app.route('/login/', methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/blogs')
-def blogs():
-    return render_template('blogs.html')
+@app.route('/blogs/<int:id>/')
+def blogs(id):
+    return render_template('blogs.html', blog_id = id)
 
-@app.route('/register')
+@app.route('/register/', methods=['GET','POST'] )
 def register():
     return render_template('register.html')
 
 
-@app.route('/editblogs')
+@app.route('/editblogs/<int:id>/', methods=['GET','POST'])
 def editblogs():
     return render_template('editblogs.html')
 
-@app.route('/deleteblogs')
+@app.route('/deleteblogs/<int:id>/')
 def deleteblogs():
     return render_template('deleteblogs.html')
 
-@app.route('/myblogs')
+@app.route('/myblogs/')
 def myblogs():
     return render_template('myblogs.html')
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     return render_template('logout.html')
 
-@app.route('/writeblog')
+@app.route('/writeblog/', methods=['GET','POST'])
 def writeblog():
     return render_template('writeblog.html')
 
