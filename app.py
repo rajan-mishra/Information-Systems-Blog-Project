617a45064c923e0dbf9339dbd2fc7204a8a7814f
rajan-mishra <56820192+rajan-mishra@users.noreply.github.com>
2019-12-30 16:29:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,37 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/blogs')
+def blogs():
+    return render_template('blogs.html')
+
+@app.route('/editblogs')
+def editblogs():
+    return render_template('editblogs.html')
+
+@app.route('/deleteblogs')
+def deleteblogs():
+    return render_template('deleteblogs.html')
+
+@app.route('/myblogs')
+def myblogs():
+    return render_template('myblogs.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+@app.route('/writeblog')
+def writeblog():
+    return render_template('writeblog.html')
+
 if __name__ == '__main__' :
     app.run(debug = True)
